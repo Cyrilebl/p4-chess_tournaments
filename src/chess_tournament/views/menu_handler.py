4f6ba47cb8ee00391c4ecb84a07517518b394bb0
Cyril Leblanc <cyril.leblanc@outlook.com>
2024-12-18 11:28:45 +0100

    improved new player for tournament

--- src/chess_tournament/views/menu_handler.py
+++ src/chess_tournament/views/menu_handler.py
@@ -10,22 +10,11 @@ class MenuHandler:
     PLAYER_FILE = "players.json"
 
     def __init__(self):
-        self.menu_manager = MenuManager()
-
-    def load_data(self, file_name):
-        data = self.menu_manager.load_data(self.FOLDER_PATH, file_name)
-        return data if data is not None else []
-
-    def save_data(self, file_name, data):
-        self.menu_manager.save_to_json(self.FOLDER_PATH, file_name, data)
-
-    def generate_new_id(self, data):
-        existing_ids = [item["id"] for item in data]
-        return max(existing_ids, default=0) + 1
+        self.menu_manager = MenuManager(self.FOLDER_PATH)
 
     def handle_tournaments_menu(self, user_choice):
         menu = Menu()
-        tournaments_data = self.load_data(self.TOURNAMENT_FILE)
+        tournaments_data = self.menu_manager.load_data(self.TOURNAMENT_FILE)
 
         match user_choice:
             case "1":
@@ -36,7 +25,7 @@ class MenuHandler:
 
             case "2":
                 # Créer un tournoi
-                id = self.generate_new_id(tournaments_data)
+                id = self.menu_manager.generate_new_id(tournaments_data)
                 name = input("Nom: ")
                 place = input("Lieu: ")
                 start_date = input("Date de début (JJ/MM/AAAA): ")
@@ -50,7 +39,7 @@ class MenuHandler:
                     id, name, place, start_date, end_date, turn, description
                 )
                 tournaments_data.append(tournament.add_tournament())
-                self.save_data(self.TOURNAMENT_FILE, tournaments_data)
+                self.menu_manager.save_data(self.TOURNAMENT_FILE, tournaments_data)
 
                 return "Le tournoi a été ajouté avec succès."
 
@@ -82,7 +71,7 @@ class MenuHandler:
                 return None
 
     def handle_players_menu(self, user_choice):
-        players_data = self.load_data(self.PLAYER_FILE)
+        players_data = self.menu_manager.load_data(self.PLAYER_FILE)
 
         match user_choice:
             case "1":
@@ -93,14 +82,14 @@ class MenuHandler:
 
             case "2":
                 # Ajouter un joueur
-                id = self.generate_new_id(players_data)
+                id = self.menu_manager.generate_new_id(players_data)
                 first_name = input("Prénom du joueur: ")
                 last_name = input("Nom de famille: ")
                 birth_date = input("Date de naissance (JJ/MM/AAAA): ")
 
                 player = Player(id, first_name, last_name, birth_date)
                 players_data.append(player.add_player())
-                self.save_data(self.PLAYER_FILE, players_data)
+                self.menu_manager.save_data(self.PLAYER_FILE, players_data)
 
                 return "Le joueur a été ajouté avec succès."
 
@@ -108,7 +97,7 @@ class MenuHandler:
                 return None
 
     def handle_tournament_management(self, user_choice, id):
-        tournaments_data = self.load_data(self.TOURNAMENT_FILE)
+        tournaments_data = self.menu_manager.load_data(self.TOURNAMENT_FILE)
         selected_tournament = next(t for t in tournaments_data if t["id"] == id)
 
         match user_choice:
@@ -122,35 +111,49 @@ class MenuHandler:
 
             case "2":
                 # Ajouter des joueurs au tournoi
-                players_data = self.load_data(self.FOLDER_PATH, self.PLAYER_FILE)
+                players_data = self.menu_manager.load_data(self.PLAYER_FILE)
                 if players_data is None:
                     return "Aucun joueur enregistré."
-
                 print(display_players(players_data))
 
-                # Sélectionner un joueur
-                new_player_id = input("Entrez l'ID du joueur à ajouter: ")
+                while True:
+                    # Sélectionner un joueur
+                    new_player_id = input(
+                        "Entrez l'ID du joueur à ajouter (ou 'q' pour quitter): "
+                    )
 
-                # Trouver le joueur correspondant
-                player = next(
-                    (p for p in players_data if p["id"] == int(new_player_id)), None
-                )
-                if not player:
-                    return "Joueur introuvable."
+                    if new_player_id.lower() == "q":
+                        break
 
-                # Ajouter le joueur au tournoi
-                if "players" not in selected_tournament:
-                    selected_tournament["players"] = []
+                    try:
+                        # Trouver le joueur correspondant
+                        player = next(
+                            (p for p in players_data if p["id"] == int(new_player_id)),
+                            None,
+                        )
+                    except ValueError:
+                        print("Veuillez entrer un ID valide.")
+                        continue
 
-                # Empêcher les doublons
-                if player not in selected_tournament["players"]:
-                    selected_tournament["players"].append(player)
-                else:
-                    return "Ce joueur fait déjà partie du tournoi."
+                    if not player:
+                        print("Joueur introuvable. Veuillez réessayer.")
+                        continue
 
-                self.save_data(self.TOURNAMENT_FILE, tournaments_data)
+                    # Ajouter le joueur au tournoi
+                    if "players" not in selected_tournament:
+                        selected_tournament["players"] = []
 
-                return f"{player['first_name']} {player['last_name']} a été ajouté au tournoi."
+                    # Empêcher les doublons
+                    if player not in selected_tournament["players"]:
+                        selected_tournament["players"].append(player)
+                        print(
+                            f"{player['first_name']} {player['last_name']} a été ajouté au tournoi."
+                        )
+                    else:
+                        print("Ce joueur fait déjà partie du tournoi.")
+
+                self.menu_manager.save_data(self.TOURNAMENT_FILE, tournaments_data)
+                return "Ajout de joueurs terminé."
 
             case "3":
                 # Générer les matchs pour le tour actuel
