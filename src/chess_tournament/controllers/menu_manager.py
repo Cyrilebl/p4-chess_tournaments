b6f3bfb45dcd7b74c9828bbd873ed8079b992143
Cyril Leblanc <cyril.leblanc@outlook.com>
2024-12-18 11:27:48 +0100

    refactoring

--- src/chess_tournament/controllers/menu_manager.py
+++ src/chess_tournament/controllers/menu_manager.py
@@ -3,24 +3,29 @@ import json
 
 
 class MenuManager:
-    def get_file_path(self, folder_path, file_name):
+    def __init__(self, folder_path):
+        self.folder_path = folder_path
+
+    def get_file_path(self, file_name):
         # Ensure the folder exists
-        os.makedirs(folder_path, exist_ok=True)
+        os.makedirs(self.folder_path, exist_ok=True)
 
         # Construct the full file path
-        return os.path.join(folder_path, file_name)
+        return os.path.join(self.folder_path, file_name)
 
-    def load_data(self, folder_path, file_name):
-        full_path = self.get_file_path(folder_path, file_name)
-
+    def load_data(self, file_name):
+        full_path = self.get_file_path(file_name)
         if not os.path.exists(full_path):
-            return None
+            return []
         else:
             with open(full_path, "r") as file:
                 return json.load(file)
 
-    def save_to_json(self, folder_path, file_name, data):
-        full_path = self.get_file_path(folder_path, file_name)
-
+    def save_data(self, file_name, data):
+        full_path = self.get_file_path(file_name)
         with open(full_path, "w") as file:
             json.dump(data, file, indent=4)
+
+    def generate_new_id(self, data):
+        existing_ids = [item["id"] for item in data]
+        return max(existing_ids, default=0) + 1
