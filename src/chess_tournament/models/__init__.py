294494507d9b5fa1b15a52759684cb8bc42d6879
Cyril Leblanc <cyril.leblanc@outlook.com>
2025-01-06 17:19:40 +0100

    file organization

--- src/chess_tournament/models/__init__.py
+++ src/chess_tournament/models/__init__.py
@@ -2,7 +2,6 @@ from .player import Player
 from .tournament import Tournament
 from .turn import Turn
 from .data_manager import DataManager
-from .turn_manager import TurnManager
 
 
-__all__ = ["Player", "Tournament", "Turn", "DataManager", "TurnManager"]
+__all__ = ["Player", "Tournament", "Turn", "DataManager"]
